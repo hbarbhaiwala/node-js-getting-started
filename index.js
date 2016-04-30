ea1e1e8a358376c27ccd56effc03e491c08be37c
Husain Barbhaiwala <Husain Barbhaiwala>
2016-04-30 02:01:46 -0700

    adding a hello route

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-
+app.get('/hello', function(request, response) {
+  response.render('pages/hello');
+});
